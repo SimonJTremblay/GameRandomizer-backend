675530c7335af2f5132ce0f7e458871a58cebfc4
SimonJTremblay <sjomp103@gmail.com>
2020-05-06 16:03:24 -0400

    modified collection model and removes gameCollection.js

--- models/collections.js
+++ models/collections.js
@@ -1,15 +1,35 @@
 const mongoose = require('mongoose')
+let Schema = mongoose.Schema
 
-const collectionSchema = new mongoose.Schema({
-    user: {
-        type: Object,
-        required: true,
+const gameSchema = new Schema({   
+    bggId:   Number,
+    title:  {type: String, required:true},
+    yearPublished: Number ,
+    media:{
+        thumbnail: String,
+        image: String
     },
-    gameList: {
-        type: Array,
+    players:{
+        min: {type: Number, min: 0},
+        max: {type: Number}
+    },
+    playtime:{
+        min: {type: Number, min: 0},
+        max: {type: Number}
+    },
+    expansions: [{id: Number, value: String, owned: {type: Boolean, default: false} }],    
+    categories: [{id: Number, value: String}],
+    mechanics:  [{id: Number, value: String}],
+})
+
+const collectionSchema = new Schema({
+    userId: {
+        type: String,
         required: true,
-    }
+        unique: true
+    },
+    gameList: [gameSchema]
 })
 
 // exporting collection schema
-module.exports = mongoose.model('Collection', collectionSchema)
+module.exports =  mongoose.model('Collection', collectionSchema)
