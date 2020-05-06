4ccf3a167c8e38b4e599e71caee216f9a09a4afa
SimonJTremblay <sjomp103@gmail.com>
2020-05-06 16:02:36 -0400

    Added CORS to server

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const bodyParser= require('body-parser')
-const app = express();
 const mongoose = require('mongoose')
+const cors = require('cors');
 
 require('dotenv').config()
 
+const app = express();
+app.use(express.json())
+app.use(cors());
+
 app.use(bodyParser.urlencoded({extended: true}))
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to database'))
 
-app.use(express.json())
 
-const gameCollectionRouter = require('./routes/gameCollection')
+const gameCollectionRouter = require('./routes/collection-router')
 app.use('/Collection', gameCollectionRouter)
+
+const userRouter = require('./routes/user-router')
+app.use('/User', userRouter)
 
 app.listen(3000, () => console.log('server started'))
